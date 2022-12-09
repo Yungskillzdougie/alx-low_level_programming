92022afc3fc13fcb98e2f48ebd5331a15bddf7c0
Yungskillzdougie <douglaskache@gmail.com>
2022-12-09 02:36:01 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
-int main()
+int main(main block)
+
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle\n");
 	return 0;
 }
