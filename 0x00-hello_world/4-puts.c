72bbdfeec55d4d863f357af70fc1ca8510f54efd
Yungskillzdougie <douglaskache@gmail.com>
2022-12-09 04:19:28 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
-#include<stdio.h>
-/**
- * main -My first code line using puts
- *
- * Return: 0.
-*/
+#include <stdio.h>
 
+/**
+ *main -Prints"Progamming is like building a multilingual
+* puzzle, followed by a new line.
+ *
+ * Return: Alwys 0.
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-};
+}
