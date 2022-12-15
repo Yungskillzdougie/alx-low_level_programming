c02bb80273e36271965a24a9b77ba5832e284954
Yungskillzdougie <douglaskache@gmail.com>
2022-12-14 20:12:25 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ *
+ * print_alphabet - prints alphabet in the lower case folllowed by new line
+ *
+*/
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
