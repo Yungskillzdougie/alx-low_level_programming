ea2e09ed835213c8a66292ebb4834a1ad1ee9bed
Yungskillzdougie <douglaskache@gmail.com>
2022-12-19 04:04:43 -0800

    readme.md

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-
-
 /**
  * main - tests function that prints if integer is positive or negative
  *
@@ -9,22 +7,13 @@
  *
  */
 
-
-
 int main(void)
 
 {
 
 		int i;
 
-
-
-			i = 0;
-
-				positive_or_negative(i);
-
-
-
-					return (0);
-
+		i = 0;
+		positive_or_negative(i);
+		return (0);
 }
